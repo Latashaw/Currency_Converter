1f8fa0f406c79e94e90d4093a49666c4173d7d97
Latasha Watson <latashawatson@zipcoders-MacBook-Pro-7.local>
2017-02-21 11:37:17 -0500

    first commit

---
+++ src/main/java/latasha/jeriah/CurrencyConverter.java
@@ -0,0 +1,31 @@
+package latasha.jeriah;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by latashawatson on 2/16/17.
+ */
+public class CurrencyConverter {
+    Map<String, Double> conversionRates = new HashMap<>();
+
+    public CurrencyConverter(){
+        conversionRates.put("Us Dollar", 1.00);
+        conversionRates.put("Euro", .94);
+        conversionRates.put("British Pound", .82);
+        conversionRates.put("Indian Rupee", 68.32);
+        conversionRates.put("Australian Dollar", 1.35);
+        conversionRates.put("Canadian Dollar", 1.32);
+        conversionRates.put("Singapore Dollar", 1.43);
+        conversionRates.put("Swiss Franc", 1.01);
+        conversionRates.put("Malaysian Ringgit", 4.47);
+        conversionRates.put("Japanese Yen", 115.84);
+        conversionRates.put("Chinese Yuan", 6.92);
+    }
+
+    public double convert(String baseRate, String desiredRate, double amount){
+        double desiredAmount= Math.floor((conversionRates.get(desiredRate) * amount)/conversionRates.get(baseRate)*100);
+                                return desiredAmount/100;
+
+    }
+}
---
+++ src/test/java/latasha/jeriah/CurrencyConverterTest.java
@@ -0,0 +1,71 @@
+package latasha.jeriah;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by latashawatson on 2/16/17.
+ */
+public class CurrencyConverterTest {
+    CurrencyConverter c1;
+
+    @Before
+    public void setUp() {
+        c1 = new CurrencyConverter();
+    }
+
+    @Test
+    public void dollarToEuroTest(){
+        assertEquals(0.94, c1.convert("Us Dollar", "Euro", 1.00), 0.0);
+
+    }
+    @Test
+    public void EuroToDollarTest(){
+        assertEquals(1.06, c1.convert("Euro", "Us Dollar", 1.00), 0.0);
+
+    }
+    @Test
+    public void EuroToPoundTest(){
+        assertEquals(.87, c1.convert("Euro", "British Pound", 1.00), 0.0);
+
+    }
+    @Test
+    public void PoundToRupeeTest(){
+        assertEquals(83.31, c1.convert("British Pound", "Indian Rupee", 1.00), 0.0);
+
+    }
+    @Test
+    public void IndianRupeeToCanadianDollarTest(){
+        assertEquals(.02, c1.convert("Indian Rupee", "Canadian Dollar", 1.00), 0.01);
+
+    }
+    @Test
+    public void CanadianToSingaporeDollarTest(){
+        assertEquals(1.08, c1.convert("Canadian Dollar", "Singapore Dollar", 1.00), 0.00);
+
+    }
+    @Test
+    public void SingaporeDollarToSwissFrancTest(){
+        assertEquals(.70, c1.convert("Singapore Dollar", "Swiss Franc", 1.00), 0.01);
+
+    }
+    @Test
+    public void SwissFrancToMalaysianRinggitTest(){
+        assertEquals(4.42, c1.convert("Swiss Franc", "Malaysian Ringgit", 1.00), 0.01);
+
+    }
+    @Test
+    public void MalaysianRinggitToJapaneseYenTest(){
+        assertEquals(25.91, c1.convert("Malaysian Ringgit", "Japanese Yen", 1.00), 0.01);
+
+    }
+    @Test
+    public void JapaneseYenToChineseYuanTest(){
+        assertEquals(.05, c1.convert("Japanese Yen", "Chinese Yuan", 1.00), 0.01);
+
+    }
+
+
+}
